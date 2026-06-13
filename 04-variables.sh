5c2880e4b41e85d31306360a753b74b8993c034a
Vinay120398 <vinaychunduru98@gmail.com>
2026-06-13 21:38:32 +0530

    updated variables_new1

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PLAYER1=KHOLI
-PLAYER2=ROHIT
-KEYWORD1=wellplayed 
+PLAYER1=$1
+PLAYER2=$2
+KEYWORD1=$3 
 
 echo "$PLAYER1:: Hi $PLAYER2, $KEYWORD1 what an innings"
 echo "$PLAYER2:: Hi $PLAYER1, $KEYWORD1 thankyou"
