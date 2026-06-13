dfa6c56e481c2ce97b48d1e206e70d57568dd978
Vinay120398 <vinaychunduru98@gmail.com>
2026-06-13 21:27:23 +0530

    updated variables_new

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,6 +2,7 @@
 
 PLAYER1=VIRAT
 PLAYER2=VINAY
+KEYWORD1= wellplayed 
 
-echo "$PLAYER1:: Hi $PLAYER2, well played what an innings"
-echo "$PLAYER2:: Hi $PLAYER1, well played thankyou"
+echo "$PLAYER1:: Hi $PLAYER2, $KEYWORD1 what an innings"
+echo "$PLAYER2:: Hi $PLAYER1, $KEYWORD1 thankyou"
