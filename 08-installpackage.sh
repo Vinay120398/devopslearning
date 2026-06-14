f7f69e12f74901adc12e7f507c47f632e9993fe1
Vinay120398 <vinaychunduru98@gmail.com>
2026-06-14 11:00:09 +0530

    updated variables_new1

--- 08-installpackage.sh
+++ 08-installpackage.sh
@@ -2,9 +2,9 @@
 
 USERID=$(id -u)
 
-if[ $USERID -ne 0]
+if[ $USERID -ne 0 ]
 then 
-    echo "proceed with script"
+    echo "proceed with script as root user"
     exit 5 # manually exit the script when error comes
 else
     echo "you are super user"
