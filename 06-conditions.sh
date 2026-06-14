51c17ce34a6c014e73a0c7e700cbdde0e96ad270
Vinay120398 <vinaychunduru98@gmail.com>
2026-06-14 07:51:15 +0530

    updated variables_new1

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -5,7 +5,8 @@ Number=$1
 if [ $Number -gt 10]
 
 then 
-    echo(given number is greaterthan 10)
+    echo "given number is greaterthan 10"
 
 else 
-    echo(given number is lessthan 10)
+    echo "given number is lessthan 10"
+fi
