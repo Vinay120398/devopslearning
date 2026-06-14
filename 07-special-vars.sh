55e5fd33e6e779220813b44cab9d210c25734107
Vinay120398 <vinaychunduru98@gmail.com>
2026-06-14 10:46:38 +0530

    updated variables_new1

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -8,5 +8,6 @@ echo "Home directory of current user : $HOME"
 echo "which user is running this script : $USER"
 echo "hostname : $HOSTNAME"
 echo "process id of current script : $$"
+echo "to check the status of previous command : $?" # if its "0" success ; other than "0" is failure upto 1 to 127.
 sleep 50 &
 echo "process id for last command : $!"
